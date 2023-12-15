6e006f287350906c61a9f60b77928f7bdc489e22
Nina <labferment10@gmail.com>
2023-12-15 22:13:40 +0100

    added providers

---
+++ src/providers/app.tsx
@@ -0,0 +1,30 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '../lib/react-query';
+import { ReactNode } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { IS_DEVELOPMENT } from '@/config/constants';
+import { ErrorBoundary } from 'react-error-boundary';
+import Navbar from '@/components/common/Navbar';
+import Footer from '@/components/common/Footer';
+
+export type AppProviderProps = {
+  children: ReactNode;
+};
+
+export const AppProvider = ({
+  children,
+}: AppProviderProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Navbar />
+      <ErrorBoundary
+        fallback={<div>Something went wrong....:( </div>}
+        onError={console.error}
+      >
+        {children}
+      </ErrorBoundary>
+      <Footer />
+      {IS_DEVELOPMENT && <ReactQueryDevtools />}
+    </QueryClientProvider>
+  );
+};
