acb7898b34b0d527e0626b371f4338ceb4fc521e
Nina <labferment10@gmail.com>
2024-04-28 18:14:16 +0200

    fixed issues

--- src/features/articles/api/get-articles.ts
+++ src/features/articles/api/get-articles.ts
@@ -2,6 +2,7 @@ import { BASE_URL_API, LIMIT } from '@/config/constants';
 import { apiClient } from '@/lib/api-client';
 import { useQuery } from '@tanstack/react-query';
 import { ArticlesType } from '../types';
+import storage from '@/utils/storage';
 
 // Get most recent articles globally. Use query parameters to filter results. Auth is optional
 export const getArticles = (
@@ -9,26 +10,44 @@ export const getArticles = (
   author?: string,
   page?: number
 ): Promise<ArticlesType | null> => {
+  // check if the user is logged in or not
   // get articles by author
+  const user = storage.getUser()?.user;
+
   if (author) {
     return apiClient.get(
       `${BASE_URL_API}/articles?author=${encodeURIComponent(
         author
-      )}&offset=${page}&limit=${LIMIT}`
+      )}&offset=${page}&limit=${LIMIT}`,
+      {
+        headers: {
+          Authorization: `Token ${user && user.token}`,
+        },
+      }
     );
   }
 
   // get articles by tags
   if (tag) {
     return apiClient.get(
-      `${BASE_URL_API}/articles?tag=${tag}&offset=${page}&limit=${LIMIT}`
+      `${BASE_URL_API}/articles?tag=${tag}&offset=${page}&limit=${LIMIT}`,
+      {
+        headers: {
+          Authorization: `Token ${user && user.token}`,
+        },
+      }
     );
   }
 
   // global articles
 
   return apiClient.get(
-    `${BASE_URL_API}/articles?&offset=${page}&limit=${LIMIT}`
+    `${BASE_URL_API}/articles?&offset=${page}&limit=${LIMIT}`,
+    {
+      headers: {
+        Authorization: `Token ${user && user.token}`,
+      },
+    }
   );
 };
 
