72d9cf92ecafbbb88797cf1d82958e075f529a12
Nina <labferment10@gmail.com>
2024-05-03 12:22:11 +0200

    added unit test for notifications

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,10 @@
 export const IS_DEVELOPMENT =
   process.env.NODE_ENV === 'development';
 
+export const IS_TEST = process.env.NODE_ENV === 'test';
+export const IS_PRODUCTION =
+  process.env.NODE_ENV === 'production';
+
 export const APP_NAME = 'conduit';
 
 export const BASE_URL_API =
