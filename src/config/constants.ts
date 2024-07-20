b905b9254ec971796dc767c1b23979058ee9b29f
Nina <labferment10@gmail.com>
2024-07-20 23:53:42 +0200

    updated BASE_URL constant

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -9,6 +9,8 @@ export const APP_NAME = 'conduit';
 
 export const BASE_URL_API =
   'https://api.realworld.io/api';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = IS_DEVELOPMENT
+  ? 'http://localhost:3000'
+  : 'https://realworld-app-alpha.vercel.app';
 
 export const LIMIT = 20;
